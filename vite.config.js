dff43b7c02fc25baf44483f094a888bf0b451cce
Tomiwajin <67241225+Tomiwajin@users.noreply.github.com>
2025-06-03 11:40:50 -0400

    Configured src path to be @

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "patht"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(_dirname, "./src"),
+      
+    }
+  }
 })
