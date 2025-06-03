a8201d06e876cc4e3c5328860d99595037f7d8ab
Tomiwajin <67241225+Tomiwajin@users.noreply.github.com>
2025-06-03 12:48:35 -0400

    Created Pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+export const Home = () =>{
+
+    return (
+         <div className= "min-h-screen">
+
+        {/*Load screen */}
+
+        {/* Theme Toggle */}
+
+        {/* BackGround Effecct Toggle */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* footer  */}
+
+
+
+
+    </div>
+    )
+}
