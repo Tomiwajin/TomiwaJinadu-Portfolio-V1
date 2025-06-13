ac57a43e21551914c16509ac0d035958475dc0df
Tomiwajin <67241225+Tomiwajin@users.noreply.github.com>
2025-06-12 20:34:59 -0400

    Added Resume

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -42,7 +42,7 @@ export const AboutSection = () => {
                 Get in touch
               </a>
               <a
-                href=""
+                href="/SoftwareDev_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download Resume
